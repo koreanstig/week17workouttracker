f089935e8d1d0113cabaa7f851881b5bf14230b1
Jung David Lee <jungdavidlee@gmail.com>
2021-04-21 15:26:55 -0700

    added stats route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,4 +6,8 @@ router.get("/exercise", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const htmlRoutes = require("./routes/htmlRoutes")
 const apiRoutes = require("./routes/apiRoutes.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(morgan("dev"));
